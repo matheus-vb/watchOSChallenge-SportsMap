01f8ae458038b34859fdac2c11aad88f54930c9b
João Pedro <jpcm2@cin.ufpe.br>
2022-11-30 14:09:28 -0300

    feature(mapView): modularizing the location list

--- Qatar-Challenge/Qatar-Challenge WatchKit Extension/Views/MapView.swift
+++ Qatar-Challenge/Qatar-Challenge WatchKit Extension/Views/MapView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region =  MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
     let locations: [Location]
+    
+    @State private var region =  MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
         
     var body: some View {
         Map(coordinateRegion: $region, annotationItems: locations){location in
