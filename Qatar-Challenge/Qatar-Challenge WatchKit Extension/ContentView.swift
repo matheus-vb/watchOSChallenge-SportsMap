89e15ed15ad556d9751fbc4f3eeb28debda5c103
João Pedro <jpcm2@cin.ufpe.br>
2022-12-02 14:21:48 -0300

    feature(MapViewController): using mapviewcontroller in the content view

--- Qatar-Challenge/Qatar-Challenge WatchKit Extension/ContentView.swift
+++ Qatar-Challenge/Qatar-Challenge WatchKit Extension/ContentView.swift
@@ -8,18 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let mapViewController = MapViewController()
+
     var body: some View {
-        MapScreen(locations: [
-            Location(name: "Arena Recife",
-                                             coordinate: CLLocationCoordinate2D(latitude: -8.042511416608455, longitude: -34.92634404282216),
-                                             sport: PinSports.futebol,
-                                             category: "Campo de Futebol",
-                                             free: false,
-                                             address: "R. Álvares Píres, 16 - Cordeiro",
-                                             time: "07-23",
-                                             asset: "futebolBlue",
-                                             distance: nil)
-        ])
+        MapView(locations: mapViewController.setupList())
+            .padding()
     }
 }
 
