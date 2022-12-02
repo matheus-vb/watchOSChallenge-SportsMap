fadf34ac44d88b04604f79cc9e69b7a0c0c90803
João Pedro <jpcm2@cin.ufpe.br>
2022-12-02 14:18:02 -0300

    feature(MapViewController): MapViewController protocol + Class MapViewController

---
+++ Qatar-Challenge/Qatar-Challenge WatchKit Extension/Protocols/MapViewControllerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewControllerProtocol.swift
+//  Qatar-Challenge WatchKit Extension
+//
+//  Created by jpcm2 on 02/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+protocol MapViewControllerProtocol{
+    func setupList() -> [Location]
+}
