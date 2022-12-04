7bcf81742275b5b70662f067b4612162c0a5f8fb
João Pedro <jpcm2@cin.ufpe.br>
2022-12-04 15:31:42 -0300

    feature(mapNavigation): navigation added

--- Qatar-Challenge/Qatar-Challenge WatchKit Extension/Views/ResultsTableView.swift
+++ Qatar-Challenge/Qatar-Challenge WatchKit Extension/Views/ResultsTableView.swift
@@ -11,21 +11,22 @@ struct ResultsTableView: View {
     
     let sportType: String
     let models = LocationStore()
+    let mapViewController = MapViewController()
     
     var body: some View {
-        ZStack {
-            VStack{
-                Spacer()
-                    .frame(height: 8)
-                SeeOnMapView()
-                Spacer()
-                    .frame(height: 8)
-                ScrollView{
-                    VStack(spacing: 8){
-                        ForEach(models.modelDict[sportType]!) { loc in
-                            SeeAllComponent(image: loc.asset, localName: loc.name, type: loc.category, isfree: loc.free, distance: "500")
-                                .listRowBackground(Color.clear)
-                        }
+        NavigationView{
+            ScrollView(showsIndicators: false){
+                NavigationLink{
+                    MapView(locations: mapViewController.setupList())
+                }label:{
+                    SeeOnMapView()
+                }.buttonStyle(
+                    PlainButtonStyle()
+                )
+                VStack(spacing: 8){
+                    ForEach(models.modelDict[sportType]!) { loc in
+                        SeeAllComponent(image: loc.asset, localName: loc.name, type: loc.category, isfree: loc.free, distance: "500")
+                            .listRowBackground(Color.clear)
                     }
                 }
             }
