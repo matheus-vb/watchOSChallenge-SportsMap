33b0d78dd0a74250b05940a508648bfc4a0b7760
João Pedro <jpcm2@cin.ufpe.br>
2022-12-04 15:04:58 -0300

    feature(seeOnMap): seeOnMap Button added

--- Qatar-Challenge/Qatar-Challenge WatchKit Extension/Views/ResultsTableView.swift
+++ Qatar-Challenge/Qatar-Challenge WatchKit Extension/Views/ResultsTableView.swift
@@ -14,11 +14,18 @@ struct ResultsTableView: View {
     
     var body: some View {
         ZStack {
-            ScrollView{
-                VStack(spacing: 8){
-                    ForEach(models.modelDict[sportType]!) { loc in
-                        SeeAllComponent(image: loc.asset, localName: loc.name, type: loc.category, isfree: loc.free, distance: "500")
-                            .listRowBackground(Color.clear)
+            VStack{
+                Spacer()
+                    .frame(height: 8)
+                SeeOnMapView()
+                Spacer()
+                    .frame(height: 8)
+                ScrollView{
+                    VStack(spacing: 8){
+                        ForEach(models.modelDict[sportType]!) { loc in
+                            SeeAllComponent(image: loc.asset, localName: loc.name, type: loc.category, isfree: loc.free, distance: "500")
+                                .listRowBackground(Color.clear)
+                        }
                     }
                 }
             }
